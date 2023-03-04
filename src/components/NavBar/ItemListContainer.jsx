f8a38ee98158531013977881f0cd27b088c0268b
Maximo Pavesi <machipavesi@gmail.com>
2023-03-04 13:16:13 -0300

    subiendo itemlistcontainer

---
+++ src/components/NavBar/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Children } from "react";
+import "./ItemListContainer.css";
+
+const ItemListContainer = (props) => {
+    return (
+        <div>
+            <h1>No hay articulos en la tienda todavia</h1>
+        </div>
+    );
+};
+
+export default ItemListContainer;
