0aebd5b2fc66dd0c37bd5904b3c110b788533c92
Maximo Pavesi <machipavesi@gmail.com>
2023-03-04 13:21:54 -0300

    subiendo cartwidget

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import {NavBar} from "./components/NavBar/NavBar";
+import CartWidget from './components/CartWidget/CartWidget';
+import ItemListContainer from "./components/NavBar/ItemListContainer";
 
 function App() {
   return (
     <>
       <NavBar/>
+      <CartWidget/>
+      <ItemListContainer/>
     </>
   );
 }
